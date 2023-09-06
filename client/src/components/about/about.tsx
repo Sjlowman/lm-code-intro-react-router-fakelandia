89e61752a69b67629cef481134e150bf6c7101a5
Sjlowman <sarah.j.lowman@gmail.com>
2023-09-06 16:01:33 +0100

    Basic formatting improvement for Help and About

--- client/src/components/about/about.tsx
+++ client/src/components/about/about.tsx
@@ -1,13 +1,15 @@
 const About: React.FC = () => (
-  <section className="content">
-    <h2 className="subtitle">About This Site</h2>
-    <p>This site only wants the best for its citizens</p>
-    <p>Some people say that we live in a nanny state</p>
-    <p>These people obviously have something to hide</p>
-    <p>Don't be one of those people</p>
-    <p>
-      Confess your crime <strong>TODAY</strong>.
-    </p>
+  <section className="text-center text-3xl">
+    <h2>About This Site</h2>
+    <div className="text-center text-2xl">
+      <p>This site only wants the best for its citizens</p>
+      <p>Some people say that we live in a nanny state</p>
+      <p>These people obviously have something to hide</p>
+      <p>Don't be one of those people</p>
+      <p>
+        Confess your crime <strong>TODAY</strong>.
+      </p>
+    </div>
   </section>
 );
 
--- client/src/components/help/help.tsx
+++ client/src/components/help/help.tsx
@@ -1,7 +1,9 @@
 const Help = () => (
-  <section className="content">
-    <h2 className="subtitle">We only want to help</h2>
-    <p>You will feel much better when you have got it off your chest</p>
+  <section className="text-center text-3xl">
+    <h2>We only want to help</h2>
+    <p className="text-center text-2xl">
+      You will feel much better when you have got it off your chest
+    </p>
   </section>
 );
 
